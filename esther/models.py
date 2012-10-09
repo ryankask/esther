36c265a242833529dc3f2098fd667b8097d50173
Ryan Kaskel <dev@ryankaskel.com>
2012-10-09 19:26:50 +0100

    Different User table name.

--- esther/models.py
+++ esther/models.py
@@ -8,6 +8,7 @@ def get_columns(model):
 
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(254), unique=True, nullable=False)
     full_name = db.Column(db.String(128))
