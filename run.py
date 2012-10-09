d2f0a9bd2e1ced4c58c07a86b522b13a878c420b
Ryan Kaskel <dev@ryankaskel.com>
2012-10-09 19:02:11 +0100

    Database management command.

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import sys
 
 from flask.ext.script import Manager
 
-from esther import create_app
+from esther import create_app, db
 
 app = create_app(['esther.settings.site'])
 manager = Manager(app)
@@ -27,6 +27,16 @@ def tests():
     result = run_tests()
 
     if not result:
+        sys.exit(1)
+
+@manager.command
+def database(action):
+    if action == 'create':
+        db.create_all()
+    elif action == 'drop':
+        db.drop_all()
+    else:
+        print(u'Invalid action: "{0}"'.format(action))
         sys.exit(1)
 
 if __name__ == '__main__':
