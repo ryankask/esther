5c59f5921639d403cd77decd6e35b673d76f9c3a
Ryan Kaskel <dev@ryankaskel.com>
2013-02-23 21:11:15 +0000

    Remove comments.

--- esther/static/js/app.js
+++ esther/static/js/app.js
@@ -1,4 +1,3 @@
-
 ;(function ($, window, undefined) {
   'use strict';
 
@@ -21,13 +20,6 @@
     $.fn.placeholder                ? $('input, textarea').placeholder() : null;
   });
 
-  // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
-  // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
-  // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
-  // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
-  // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
-
-  // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
   if (Modernizr.touch && !window.location.hash) {
     $(window).load(function () {
       setTimeout(function () {
