b57f446612be1d922df418e5cf06a25f3acb8a26
Ryan Kaskel <dev@ryankaskel.com>
2013-04-06 20:38:45 +0100

    Fix formatting.

--- esther/tests/angular/static/js/scenarios.js
+++ esther/tests/angular/static/js/scenarios.js
@@ -6,15 +6,14 @@ describe('todo app', function() {
     browser().navigateTo('/todo');
   });
 
-  it('the create list form should be controlled by buttons and links',
-     function() {
-       expect(element('#create-list').css('display')).toBe('none');
-       element('.options li:first a').click();
-       expect(element('#create-list').css('display')).toBe('block');
-       element('form .button.alert').click();
-       expect(element('#create-list').css('display')).toBe('none');
-       element('.options li:first a').click();
-       expect(element('#create-list').css('display')).toBe('block');
-   });
+  it('the create list form should be controlled by buttons and links', function() {
+    expect(element('#create-list').css('display')).toBe('none');
+    element('.options li:first a').click();
+    expect(element('#create-list').css('display')).toBe('block');
+    element('form .button.alert').click();
+    expect(element('#create-list').css('display')).toBe('none');
+    element('.options li:first a').click();
+    expect(element('#create-list').css('display')).toBe('block');
+  });
 
 });
