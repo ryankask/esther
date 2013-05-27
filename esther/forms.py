9c8e47d58a3bf9752c466a1c66ded39cd273b0dc
Ryan Kaskel <dev@ryankaskel.com>
2013-05-27 09:22:34 +0100

    ..

--- esther/forms.py
+++ esther/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from flask import current_app
 from flask.ext.wtf import (Form, Field, TextField, TextAreaField, PasswordField,
                            BooleanField, SelectField, DateTimeField,
@@ -19,7 +21,8 @@ def unique(column, message=None):
     def validator(form, field):
         if (field.object_data != field.data and
             db.session.query(exists().where(column == field.data)).scalar()):
-            raise ValidationError(message.format(field.label.text))
+            label_text = re.sub(r'\([^)]*\)', '', field.label.text).strip()
+            raise ValidationError(message.format(label_text))
 
     return validator
 
